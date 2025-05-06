fc5b22dd08d3d23565ec741e3fe7f58ff97eaf15
LeMarketingX <lemarketingx@gmail.com>
2025-05-06 12:49:07 +0300

    Initial commit: CreditCompass - אפליקציה פיננסית

---
+++ js/investmentCalculator.js
@@ -0,0 +1,6 @@
+export function renderInvestment(container) {
+    container.innerHTML = `
+      <h2>מחשבון השקעות</h2>
+      <p>בפיתוח…</p>
+    `;
+  }
---
+++ js/taxCalculator.js
@@ -0,0 +1,6 @@
+export function renderTax(container) {
+    container.innerHTML = `
+      <h2>מחשבון מסים</h2>
+      <p>בפיתוח…</p>
+    `;
+  }
